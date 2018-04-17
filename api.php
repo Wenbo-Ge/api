23cce1d5b4fba54c407d6972fc38ab27fa9cc3f5
Wenbo-Ge <wge19920921@gmail.com>
2018-04-17 18:15:47 +0000

    api practice

---
+++ api.php
@@ -0,0 +1,32 @@
+<?php
+header('Content-Type:application/json');
+require_once('data.php');
+
+function response($status,$status_message,$data){
+	header('HTTP/1.1'.$status);
+
+	$response['status']=$status;
+	$response['status_message']=$status_message;
+	$response['data']=$data;
+	$response['time']='18.04.16';
+
+	$json_response=json_encode($response);
+	echo $json_response;
+}
+
+if (!empty($_GET['name'])) {
+	$name=$_GET['name'];
+	$price=get_price($name);
+
+	if (empty($price)) {
+		response(200,'Product Not Found', NULL);
+	} else {
+		response(200,'Product Found', $price);
+
+	}
+	
+}else{
+	response(400,'Invalid Request', NULL);
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Rest API Client Side Demo</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Rest API Client Side Demo</h2>
+  <!-- <form class="form-inline" action="" method="POST"> -->
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" name="name" class="form-control" id="input_name" placeholder="Enter Product Name" required/>
+    </div>
+    <button type="submit" name="submit" class="btn btn-default" id="submit" onclick="search()">Submit</button>
+  <!-- </form> -->
+  <div class="content"></div>
+  <script type="text/javascript">
+
+    // $(document).ready(function(){
+
+    //   $('#submit').click(function(){
+    //     var search=$('#input_name').val();
+    //     console.log(search);
+    //   })
+    // })
+    function search(){
+        var search=$('#input_name').val();
+        console.log(search);
+        $.get(
+            'http://devvm.com/api/api.php?name='+search, function(data){
+              $('.content').html(data.data);
+            }
+          )
+        // $.post(
+        //     'http://devvm.com/api/api.php',
+        //     {name:search},
+        //     function(data){
+        //       $('.content').html(data.data);
+        //     }
+        //   )
+
+
+    }
+  </script>
+  <p>&nbsp;</p>
+
+</div>
+</body>
+</html>
